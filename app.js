afd12859daac2306fcaa3cd5b9758c7c8b85b6bb
Daniel Silva <dodosilva51@gmail.com>
2019-08-18 21:35:55 -0300

    Added Facebook Hooks, .env environment, SSL certificates and robots.txt

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +25,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Webhooks setup
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  if (body.object === 'page') {
+
+    body.entry.forEach(function(entry) {
+
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+    });
+    
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+
+    if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
+
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
